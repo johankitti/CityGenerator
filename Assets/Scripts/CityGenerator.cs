b87eb93048702202e1cc0d4b3214f07623f98837
johankitti <johan.kitti@gmail.com>
2016-09-15 17:30:42 +0200

    added some more buildings and stuff.

--- Assets/Scripts/CityGenerator.cs
+++ Assets/Scripts/CityGenerator.cs
@@ -3,14 +3,13 @@ using System.Collections;
 
 public class CityGenerator : MonoBehaviour {
 
-	public GameObject Building;
+	public GameObject[] Buildings;
 
-	int citySize = 40;
+	int citySize = 30;
 	float buildingSize = 30;
 	int[,] city;
 
 	void Start () {
-		Debug.Log (Building.transform.localScale);
 		city = new int[citySize, citySize];
 		Generate ();
 	}
@@ -21,8 +20,8 @@ public class CityGenerator : MonoBehaviour {
 				float xPos = x * buildingSize;
 				float yPos = y * buildingSize;
 				float height = Mathf.PerlinNoise (xPos / 100, yPos / 100) * 120;
-				Vector3 instPos = new Vector3 (xPos, height, yPos);
-				GameObject instBuilding = Instantiate (Building, Vector3.zero + instPos, Quaternion.identity) as GameObject;
+				Vector3 instPos = new Vector3 (xPos, 0, yPos);	
+				GameObject instBuilding = Instantiate (Buildings[Random.Range(0, Buildings.Length)], Vector3.zero + instPos, Quaternion.identity) as GameObject;
 			}
 		}
 	}
