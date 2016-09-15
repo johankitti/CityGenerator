807a995bbe2da3923c73f0d50049d46615b37f83
johankitti <johan.kitti@gmail.com>
2016-09-15 15:43:00 +0200

    basic generator.

--- Assets/Scripts/CityGenerator.cs
+++ Assets/Scripts/CityGenerator.cs
@@ -3,13 +3,27 @@ using System.Collections;
 
 public class CityGenerator : MonoBehaviour {
 
-	// Use this for initialization
+	public GameObject Building;
+
+	int citySize = 40;
+	float buildingSize = 30;
+	int[,] city;
+
 	void Start () {
-	
+		Debug.Log (Building.transform.localScale);
+		city = new int[citySize, citySize];
+		Generate ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Generate() {
+		for (int x = 0; x < citySize; x++) {
+			for (int y = 0; y < citySize; y++) {
+				float xPos = x * buildingSize;
+				float yPos = y * buildingSize;
+				float height = Mathf.PerlinNoise (xPos / 100, yPos / 100) * 120;
+				Vector3 instPos = new Vector3 (xPos, height, yPos);
+				GameObject instBuilding = Instantiate (Building, Vector3.zero + instPos, Quaternion.identity) as GameObject;
+			}
+		}
 	}
 }
