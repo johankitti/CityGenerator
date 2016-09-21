c1aa859f16c2a485c8ef312b14a2394fac1e77dc
johankitti <johan.kitti@gmail.com>
2016-09-21 11:43:19 +0200

    Started implementation of new system to spawn tiles around camera lookAt.

--- Assets/MoveCamera.cs
+++ Assets/MoveCamera.cs
@@ -5,25 +5,18 @@ public class MoveCamera : MonoBehaviour {
 
     float Speed = 80;
 
-    Transform TouchGround;
-
-    void Start() {
-        TouchGround = GetComponentInChildren<Transform>();
-        Debug.Log(TouchGround.name);
-    }
-
 	void Update () {
         Vector3 moveVector = Vector3.zero;
                 
         if (Input.GetKey(KeyCode.LeftArrow)) {
-            moveVector += new Vector3(1, 0, -1);
-        } else if (Input.GetKey(KeyCode.RightArrow)) {
             moveVector += new Vector3(-1, 0, 1);
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            moveVector += new Vector3(1, 0, -1);
         }
         if (Input.GetKey(KeyCode.UpArrow)) {
-            moveVector += new Vector3(-1, 0, -1);
-        } else if (Input.GetKey(KeyCode.DownArrow)) {
             moveVector += new Vector3(1, 0, 1);
+        } else if (Input.GetKey(KeyCode.DownArrow)) {
+            moveVector += new Vector3(-1, 0, -1);
         }
         transform.position += moveVector * Time.deltaTime * Speed;
     }
--- Assets/Scripts/CityGenerator.cs
+++ Assets/Scripts/CityGenerator.cs
@@ -3,48 +3,115 @@ using System.Collections;
 
 public class CityGenerator : MonoBehaviour {
 
+    public Transform CameraView2DPos;
+    public GameObject BasicBlock;
+    public GameObject BaseConcrete;
 	public GameObject[] Buildings;
 
-    const float buildingSize = 30;
+    const int TileSize = 60;
+    const int BasicBlockHeight = 10;
+    const int BaseConcreteHeight = 3;
+    const int TilePadding = 3;
 
-    int citySize = 40;
-	int[,] city;
+    int xPos = 0;
+    int yPos = 0;
 
-	void Start () {
-        Generate ();
-	}
+    int CitySize = 40;
 
-	void Generate() {
-		for (int x = 0; x < citySize; x++) {
-			for (int y = 0; y < citySize; y++) {
-				float xPos = x * buildingSize;
-				float yPos = y * buildingSize;
-				float height = Mathf.PerlinNoise (xPos / 100, yPos / 100) * 120;
-				Vector3 instPos = new Vector3 (xPos, 0, yPos);	
-				//Debug.Log (height);
+    int[,] CityTileMap;
+	GameObject[,] CityGameObjects;
 
-                /*
-				GameObject toInst;
+    
+    void Start() {
+        GenerateCity();
+    }
 
-				if (height < 20) {
-					toInst = Buildings [0];
-				} else if (height < 40) {
-					toInst = Buildings [1];
-				} else if (height < 60) {
-					toInst = Buildings [2];
-				} else if (height < 70) {
-					toInst = Buildings [3];
-				} else if (height < 80) {
-					toInst = Buildings [6];
-				} else {
-					toInst = Buildings [7];
-				}
-                */
+    void Update() {
+        int newXPos = (int)(CameraView2DPos.position.x / (float)TileSize + 0.5f);
+        int newYPos = (int)(CameraView2DPos.position.z / (float)TileSize + 0.5f);
 
-                Quaternion instRot = Quaternion.Euler(new Vector3(0, 90 * Random.Range(0, 3), 0));
-				GameObject instBuilding = Instantiate (Buildings[Random.Range(0, Buildings.Length)], Vector3.zero + instPos, instRot) as GameObject;
-                instBuilding.name = "Building[" + x + "," + y + "]";
-			}
+        // If position changed, generate new tiles
+        if (newXPos != xPos || newYPos != yPos) {
+            Debug.Log("NEW POS: [" + newXPos + ", " + newYPos + "]");
+
+            if (newXPos > xPos) {
+                ChangeTileArea(true, newXPos + TilePadding, newXPos + TilePadding, newYPos - TilePadding, newYPos + TilePadding);
+                ChangeTileArea(false, newXPos - TilePadding, newXPos - TilePadding, newYPos - TilePadding, newYPos + TilePadding);
+            } else if (newXPos < xPos) {
+                ChangeTileArea(true, newXPos - TilePadding, newXPos - TilePadding, newYPos - TilePadding, newYPos + TilePadding);
+                ChangeTileArea(false, newXPos + TilePadding, newXPos + TilePadding, newYPos - TilePadding, newYPos + TilePadding);
+            }
+
+            if (newYPos > yPos) {
+
+            }
+            else if (newYPos < yPos) {
+
+            }
+
+            SetColor(Color.red, newXPos, newYPos);
+            SetColor(Color.white, xPos, yPos);
+
+            xPos = newXPos;
+            yPos = newYPos;
+        }
+    }
+
+    void GenerateCity() {
+        CityGameObjects = new GameObject[CitySize, CitySize];
+
+        CityTileMap = new int[CitySize, CitySize];
+
+        for (int x = 0; x < CitySize; x++) {
+            for (int y = 0; y < CitySize; y++) {
+                CityTileMap[x, y] = 1;
+            }
+        }
+
+        ChangeTileArea(true, xPos - TilePadding, xPos + TilePadding, yPos - TilePadding, yPos + TilePadding);
+    }
+
+    void ChangeTileArea(bool spawn, int startTileX, int endTileX, int startTileY, int endTileY) {
+        startTileX = (int)Mathf.Max(0, startTileX);
+        endTileX = (endTileX > CitySize) ? CitySize : endTileX;
+
+        startTileY = (int)Mathf.Max(0, startTileY);
+        endTileY = (endTileY > CitySize) ? CitySize : endTileY;
+
+        if (spawn)
+            Debug.Log("Adding:");
+        else
+            Debug.Log("Removing:");
+        Debug.Log("X: " + startTileX + " -> " + endTileX);
+        Debug.Log("Y: " + startTileY + " -> " + endTileY);
+
+        for (int x = startTileX; x < endTileX + 1; x++) {
+			for (int y = startTileY; y < endTileY + 1; y++) {
+                ChangeTile(spawn, x, y);
+            }
 		}
 	}
+
+    void ChangeTile(bool spawn, int x, int y) {
+        if (spawn) { 
+            if (CityGameObjects[x, y] == null) { 
+                float instPosX = x * TileSize;
+                float instPosY = y * TileSize;
+
+                Vector3 basicTilePos = new Vector3(instPosX, -BasicBlockHeight, instPosY);
+                Vector3 baseConcretePos = new Vector3(instPosX, 0, instPosY);
+
+                Quaternion instRot = Quaternion.Euler(new Vector3(0, 90 * Random.Range(0, 3), 0));
+
+                CityGameObjects[x, y] = Instantiate(BasicBlock, Vector3.zero + basicTilePos, instRot) as GameObject;
+                CityGameObjects[x, y].name = "Tile[" + x + ", " + y + "]";
+            }
+        } else {
+            Destroy(CityGameObjects[x, y]);
+        }
+    }
+
+    void SetColor(Color color, int x, int y) {
+        CityGameObjects[x, y].GetComponentInChildren<Renderer>().material.color = color;
+    }
 }
