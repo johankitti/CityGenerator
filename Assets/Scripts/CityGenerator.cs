9a95221bebbdb7535c81e8faf051112e10d9d0e6
johankitti <johan.kitti@gmail.com>
2016-09-21 17:18:56 +0200

    City now is tweakable through sliders and can be generated by a click of a button.

--- Assets/Scripts/CityGenerator.cs
+++ Assets/Scripts/CityGenerator.cs
@@ -1,117 +1,184 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class CityGenerator : MonoBehaviour {
 
+    public enum District {
+        Business, Commercial, Residential, Industrial
+    }
+
     public Transform CameraView2DPos;
-    public GameObject BasicBlock;
+    public Tile TilePrefab;
     public GameObject BaseConcrete;
 	public GameObject[] Buildings;
+
+    // UI
+    public Slider NoiseDetailSlider;
+    public Slider RandomSeedSlider;
+    public Slider BusinessCommercialSlider;
+    public Slider ResidentialSlider;
+    public Slider IndustrialSlider;
+    public Slider SpreadCommercialSlider;
 
     const int TileSize = 60;
     const int BasicBlockHeight = 10;
     const int BaseConcreteHeight = 3;
-    const int TilePadding = 3;
+    const int TilePadding = 5;
 
-    int xPos = 0;
-    int yPos = 0;
+    int xPos = -9999;
+    int yPos = -9999;
 
-    int CitySize = 40;
+    int CitySize = 100;
 
-    int[,] CityTileMap;
-	GameObject[,] CityGameObjects;
+	Tile[,] CityTileMap;
 
-    
+    District[,] CityDistrictMap;
+
+    float NoiseDetailX = 10;
+    float NoiseDetailY = 10;
+
     void Start() {
         GenerateCity();
-    }
 
-    void Update() {
-        int newXPos = (int)(CameraView2DPos.position.x / (float)TileSize + 0.5f);
-        int newYPos = (int)(CameraView2DPos.position.z / (float)TileSize + 0.5f);
-
-        // If position changed, generate new tiles
-        if (newXPos != xPos || newYPos != yPos) {
-            Debug.Log("NEW POS: [" + newXPos + ", " + newYPos + "]");
-
-            if (newXPos > xPos) {
-                ChangeTileArea(true, newXPos + TilePadding, newXPos + TilePadding, newYPos - TilePadding, newYPos + TilePadding);
-                ChangeTileArea(false, newXPos - TilePadding, newXPos - TilePadding, newYPos - TilePadding, newYPos + TilePadding);
-            } else if (newXPos < xPos) {
-                ChangeTileArea(true, newXPos - TilePadding, newXPos - TilePadding, newYPos - TilePadding, newYPos + TilePadding);
-                ChangeTileArea(false, newXPos + TilePadding, newXPos + TilePadding, newYPos - TilePadding, newYPos + TilePadding);
-            }
-
-            if (newYPos > yPos) {
-
-            }
-            else if (newYPos < yPos) {
-
-            }
-
-            SetColor(Color.red, newXPos, newYPos);
-            SetColor(Color.white, xPos, yPos);
-
-            xPos = newXPos;
-            yPos = newYPos;
-        }
+        NoiseDetailSlider.value = 0.4f;
+        RandomSeedSlider.value = 0.0f;
+        BusinessCommercialSlider.value = 0.4f;
+        ResidentialSlider.value = 0.9f;
+        IndustrialSlider.value = 0.6f;
+        SpreadCommercialSlider.value = 0.25f;
     }
 
     void GenerateCity() {
-        CityGameObjects = new GameObject[CitySize, CitySize];
-
-        CityTileMap = new int[CitySize, CitySize];
+        CityTileMap = new Tile[CitySize, CitySize];
+        CityDistrictMap = new District[CitySize, CitySize];
 
         for (int x = 0; x < CitySize; x++) {
             for (int y = 0; y < CitySize; y++) {
-                CityTileMap[x, y] = 1;
-            }
-        }
-
-        ChangeTileArea(true, xPos - TilePadding, xPos + TilePadding, yPos - TilePadding, yPos + TilePadding);
-    }
-
-    void ChangeTileArea(bool spawn, int startTileX, int endTileX, int startTileY, int endTileY) {
-        startTileX = (int)Mathf.Max(0, startTileX);
-        endTileX = (endTileX > CitySize) ? CitySize : endTileX;
-
-        startTileY = (int)Mathf.Max(0, startTileY);
-        endTileY = (endTileY > CitySize) ? CitySize : endTileY;
-
-        if (spawn)
-            Debug.Log("Adding:");
-        else
-            Debug.Log("Removing:");
-        Debug.Log("X: " + startTileX + " -> " + endTileX);
-        Debug.Log("Y: " + startTileY + " -> " + endTileY);
-
-        for (int x = startTileX; x < endTileX + 1; x++) {
-			for (int y = startTileY; y < endTileY + 1; y++) {
-                ChangeTile(spawn, x, y);
-            }
-		}
-	}
-
-    void ChangeTile(bool spawn, int x, int y) {
-        if (spawn) { 
-            if (CityGameObjects[x, y] == null) { 
                 float instPosX = x * TileSize;
                 float instPosY = y * TileSize;
 
                 Vector3 basicTilePos = new Vector3(instPosX, -BasicBlockHeight, instPosY);
-                Vector3 baseConcretePos = new Vector3(instPosX, 0, instPosY);
 
-                Quaternion instRot = Quaternion.Euler(new Vector3(0, 90 * Random.Range(0, 3), 0));
-
-                CityGameObjects[x, y] = Instantiate(BasicBlock, Vector3.zero + basicTilePos, instRot) as GameObject;
-                CityGameObjects[x, y].name = "Tile[" + x + ", " + y + "]";
+                CityTileMap[x, y] = Instantiate(TilePrefab, Vector3.zero + basicTilePos, Quaternion.identity) as Tile;
+                CityTileMap[x, y].name = "Tile[" + x + ", " + y + "]";
             }
-        } else {
-            Destroy(CityGameObjects[x, y]);
         }
     }
 
-    void SetColor(Color color, int x, int y) {
-        CityGameObjects[x, y].GetComponentInChildren<Renderer>().material.color = color;
+
+    public void UpdateCity() {
+        float noiseLow = 99999;
+        float noiseHigh = 0;
+        for (int x = 0; x < CitySize; x++) {
+            for (int y = 0; y < CitySize; y++) {
+
+                float randomSeed = 100;
+                float MainNoise = Mathf.PerlinNoise(
+                    (float)x / NoiseDetailX * NoiseDetailSlider.value + randomSeed * RandomSeedSlider.value, 
+                    (float)y / NoiseDetailY * NoiseDetailSlider.value + randomSeed * RandomSeedSlider.value);
+
+                float SpreadCommercialNoise = Mathf.PerlinNoise(
+                    (float)x / 5,
+                    (float)y / 5);
+
+                if (MainNoise < noiseLow)
+                    noiseLow = MainNoise;
+
+                if (MainNoise > noiseHigh)
+                    noiseHigh = MainNoise;
+
+                float businessValue = 1 - BusinessCommercialSlider.value;
+                float commercialValue = businessValue - (1 - ResidentialSlider.value);
+                float residentialValue = commercialValue - (1 - IndustrialSlider.value);
+                float spreadCommercialValue = 1 - SpreadCommercialSlider.value;
+
+                // Coloring
+                if (MainNoise > businessValue) {
+                    SetDistrict(District.Business, Color.blue, x, y);
+                }
+                else if (MainNoise > commercialValue) {
+                    SetDistrict(District.Commercial, Color.green, x, y);
+                }
+                else if (MainNoise > residentialValue) {
+                    if (SpreadCommercialNoise > spreadCommercialValue)
+                        SetDistrict(District.Commercial, Color.green, x, y);
+                    else
+                        SetDistrict(District.Residential, Color.red, x, y);
+                } else {
+                    SetDistrict(District.Industrial, Color.white, x, y);
+                }
+            }
+        }
+
+        //Debug.Log(noiseLow + " -> " + noiseHigh);
     }
+
+    public void BuildCity() {
+        for (int x = 0; x < CitySize; x++) {
+            for (int y = 0; y < CitySize; y++) {
+                CityTileMap[x, y].Build(CityDistrictMap[x, y], 0);
+                SetTileColor(Color.white, x, y);
+            }
+        }
+    }
+
+    void SetDistrict(District district, Color color, int x, int y) {
+        SetTileColor(color, x, y);
+        CityDistrictMap[x, y] = district;
+    }
+
+    void SetTileColor(Color color, int x, int y) {
+        if (CityTileMap[x, y] != null)
+            CityTileMap[x, y].GetComponentInChildren<Renderer>().material.color = color;
+    }
+
+    /*
+        void Update() {
+            int newXPos = (int)(CameraView2DPos.position.x / (float)TileSize + 0.5f);
+            int newYPos = (int)(CameraView2DPos.position.z / (float)TileSize + 0.5f);
+
+            // If position changed, generate new tiles
+            if (newXPos != xPos || newYPos != yPos) {
+                Debug.Log("NEW POS: [" + newXPos + ", " + newYPos + "]");
+
+                xPos = newXPos;
+                yPos = newYPos;
+
+                UpdateTileArea(xPos - TilePadding, xPos + TilePadding, yPos - TilePadding, yPos + TilePadding);
+            }
+        }
+
+
+        void UpdateTileArea(int startTileX, int endTileX, int startTileY, int endTileY) {
+            startTileX = (int)Mathf.Max(0, startTileX);
+            endTileX = (endTileX > CitySize) ? CitySize : endTileX;
+
+            startTileY = (int)Mathf.Max(0, startTileY);
+            endTileY = (endTileY > CitySize) ? CitySize : endTileY;
+
+            Debug.Log("X: " + startTileX + " -> " + endTileX);
+            Debug.Log("Y: " + startTileY + " -> " + endTileY);
+
+            for (int x = startTileX; x < endTileX + 1; x++) {
+                for (int y = startTileY; y < endTileY + 1; y++) {
+                    if (x == startTileX || x == endTileX || y == startTileY || y == endTileY)
+                        ChangeTile(false, x, y);
+                    else
+                        ChangeTile(true, x, y);
+                }
+            }
+        }
+
+        void ChangeTile(bool spawn, int x, int y) {
+            if (spawn) {
+                if (!CityTileMap[x, y].isActiveAndEnabled) {
+                    CityTileMap[x, y].gameObject.SetActive(true);
+                    CityTileMap[x, y].Activate(50, 100);
+                }
+            } else {
+                CityTileMap[x, y].gameObject.SetActive(false);
+            }
+        }
+        */
 }
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+    public GameObject TileMesh;
+
+    public GameObject[] BusinessBuildings;
+    public GameObject[] CommercialBuildings;
+    public GameObject[] ResidentialBuildings;
+    public GameObject[] IndustrialBuildings;
+
+    public void Build(CityGenerator.District district, float height) {
+        Destroy(TileMesh);
+        Quaternion rotation = Quaternion.Euler(new Vector3(0, 90 * Random.Range(0, 3), 0));
+
+        GameObject prefab;
+
+        switch(district) {
+            case (CityGenerator.District.Business):
+                prefab = BusinessBuildings[Random.Range(0, BusinessBuildings.Length - 1)];
+                break;
+            case (CityGenerator.District.Commercial):
+                prefab = CommercialBuildings[Random.Range(0, CommercialBuildings.Length - 1)];
+                break;
+            case (CityGenerator.District.Residential):
+                prefab = ResidentialBuildings[Random.Range(0, ResidentialBuildings.Length - 1)];
+                break;
+                /*
+            case (CityGenerator.District.Industrial):
+                prefab = IndustrialBuildings[Random.Range(0, IndustrialBuildings.Length - 1)];
+                break;
+                */
+            default:
+                prefab = ResidentialBuildings[Random.Range(0, ResidentialBuildings.Length - 1)];
+                break;
+        }
+        GameObject building = Instantiate(prefab, transform.localPosition, rotation) as GameObject;
+        building.transform.SetParent(transform);
+    }
+
+    /*
+    Vector3 StartPos;
+
+    public void SetStartPos() {
+        StartPos = transform.localPosition;
+    }
+
+    public void Activate(float dropHeight, float dropSpeed) {
+        transform.localPosition += Vector3.up * dropHeight;
+        StartCoroutine(Drop(dropHeight, dropSpeed));
+    }
+
+    IEnumerator Drop(float dropHeight, float dropSpeed) {
+        while(transform.localPosition.y > 0) {
+            transform.localPosition += Vector3.down * dropSpeed * Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localPosition = StartPos;
+    }
+    */
+}
